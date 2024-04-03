95ec94fe94587351f4dd716bcdde7a0eb0154753
molalenim <molaleni.mabaso@gmail.com>
2024-04-02 22:56:44 -0400

    c) Class 1 Studio: Data Types - Changed to greater than or equal to zero

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -8,18 +8,17 @@ public class Area {
         System.out.println("Enter a radius: ");
 
         if (input.hasNextDouble()) {
-            Double radius = input.nextDouble();
+            double radius = input.nextDouble();
 
-            if (radius > 0) {
-                Double area = Circle.getArea(radius);
+            if (radius >= 0) { // Changed to greater than or equal to zero
+                double area = Circle.getArea(radius);
                 System.out.println("The area of a circle with a radius of " + radius + " is: " + area);
             } else {
-                System.out.println("Enter a positive number");
-            }
-        } else {
-            System.out.println("Invalid input. Please enter a valid number.");
-        }
+                System.out.println("Enter a non-negative number");
 
-        input.close();
+                input.close();
+            }
+
+        }
     }
 }
